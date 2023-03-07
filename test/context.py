7ae405c94f22012246c85a8f4bc1c3b00aade67a
mauraisa <mauraisa@uw.edu>
2023-03-07 14:09:01 -0800

    Add additional tests.

--- test/context.py
+++ test/context.py
@@ -5,6 +5,9 @@ from hashlib import md5
 
 DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), './data'))
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import src
+
 def md5_sum(fname):
     ''' Get the md5 digest of a file. '''
     file_hash = md5()
@@ -12,7 +15,4 @@ def md5_sum(fname):
         while chunk := inF.read(8192):
             file_hash.update(chunk)
     return file_hash.hexdigest()
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import src
 
--- test/test_tsv_to_gct.py
+++ test/test_tsv_to_gct.py
@@ -9,8 +9,8 @@ from src.submodules.Tsv import Tsv
 
 class TestTsv(unittest.TestCase):
 
-    PROTEIN_TSV = f'{DATA_DIR}/protein_abundance_long.tsv'
-    PROTEIN_ANNOTATIONS = f'{DATA_DIR}/replicate_annotations.csv'
+    PROTEIN_TSV = f'{DATA_DIR}/2_reps/protein_abundance_long.tsv'
+    PROTEIN_ANNOTATIONS = f'{DATA_DIR}/2_reps/replicate_annotations.csv'
     PROTEIN_ARGS = ('ReplicateName', 'ReplicateLocator', 'ProteinAbundance')
  
     def test_can_read_tsv(self):
@@ -26,20 +26,59 @@ class TestTsv(unittest.TestCase):
             for lhs, rhs in key_value_pairs:
                 self.assertEqual(lhs, rhs)
 
-    def test_is_deterministic(self):
-
+    def test_is_deterministic_2_reps(self):
         md5s = set()
-        for i in range(50):
-            tsv = Tsv()
-            self.assertTrue(tsv.read(TestTsv.PROTEIN_TSV, *TestTsv.PROTEIN_ARGS))
-            tsv.read_annotations(TestTsv.PROTEIN_ANNOTATIONS)
-            # with tempfile.NamedTemporaryFile() as tmp:
-            # with open(, 'r') as outF:
-            fname = f'/home/ajm/code/wdl_array_tools/temp/out_{i}.gct'
-            tsv.write_gct(fname)
-            md5s.add(md5_sum(fname))
+        with tempfile.TemporaryDirectory() as tmp:
+            for i in range(50):
+                tsv = Tsv()
+                self.assertTrue(tsv.read(TestTsv.PROTEIN_TSV, *TestTsv.PROTEIN_ARGS))
+                tsv.read_annotations(TestTsv.PROTEIN_ANNOTATIONS)
+                fname = os.path.join(tmp, f'out_{i}.gct')
+                tsv.write_gct(fname)
+                md5s.add(md5_sum(fname))
         self.assertTrue(len(md5s) == 1)
-                    
+
+    def test_is_deterministic_4_reps(self):
+        protein_tsv = f'{DATA_DIR}/4_reps/subset.tsv'
+        protein_annotations = f'{DATA_DIR}/4_reps/replicate_annotations.csv'
+        md5s = set()
+        with tempfile.TemporaryDirectory() as tmp:
+            for i in range(50):
+                tsv = Tsv()
+                self.assertTrue(tsv.read(protein_tsv, 'Replicate Name', 'Replicate Locator', 'Protein Abundance'))
+                tsv.read_annotations(protein_annotations)
+                fname = os.path.join(tmp, f'out_{i}.gct')
+                tsv.write_gct(fname)
+                md5s.add(md5_sum(fname))
+        self.assertTrue(len(md5s) == 1)
+
+    def test_get_expected_result_4_reps(self):
+        protein_tsv = f'{DATA_DIR}/4_reps/subset.tsv'
+        protein_annotations = f'{DATA_DIR}/4_reps/replicate_annotations.csv'
+        target_md5 = md5_sum(f'{DATA_DIR}/4_reps/subset.gct')
+        md5s = set()
+        with tempfile.TemporaryDirectory() as tmp:
+            for i in range(50):
+                tsv = Tsv()
+                self.assertTrue(tsv.read(protein_tsv, 'Replicate Name', 'Replicate Locator', 'Protein Abundance'))
+                tsv.set_id_cols(['Protein'])
+                tsv.read_annotations(protein_annotations)
+                fname = os.path.join(tmp, f'out_{i}.gct')
+                tsv.write_gct(fname)
+                self.assertTrue(md5_sum(fname) == target_md5)
+
+    def test_get_expected_result_2_reps(self):
+        target_md5 = md5_sum(f'{DATA_DIR}/2_reps/protein_abundance_long.gct')
+        md5s = set()
+        with tempfile.TemporaryDirectory() as tmp:
+            for i in range(50):
+                tsv = Tsv()
+                self.assertTrue(tsv.read(TestTsv.PROTEIN_TSV, *TestTsv.PROTEIN_ARGS))
+                tsv.set_id_cols(['Protein'])
+                tsv.read_annotations(TestTsv.PROTEIN_ANNOTATIONS)
+                fname = os.path.join(tmp, f'out_{i}.gct')
+                tsv.write_gct(fname)
+                self.assertTrue(md5_sum(fname) == target_md5)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
